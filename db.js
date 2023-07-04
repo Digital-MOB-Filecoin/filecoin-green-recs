8b3ee3b0ebf22c59b3bf1f2e21b2308d42973a23
stefangeorgerobert <stefan_george_robert@yahoo.com>
2023-07-04 12:18:22 +0300

    filecoin-green-recs 1.0.1

--- db.js
+++ db.js
@@ -29,7 +29,7 @@ function FormatText(t) {
 export class DB {
 
     constructor() {
-        this.pool = new Pool(config.database);
+        this.pool = new Pool(Database);
     }
 
     async Query(query, log) {
@@ -43,32 +43,29 @@ export class DB {
         return result;
     }
 
-    async save_renewable_energy_from_contracts(contract) {
-        let id = contract.id;
-        let miner = contract.miner_id;
-        let totalEnergy = contract.openVolume;
+    async save_renewable_energy(data) {
+        let id = data.allocation_cid;
+        let miner = data.minerID; // ?????
+        let totalEnergy = data.allocation_volume_MWh * 1000; //MWh
 
-        let start = contract.reportingStart;
-        let date1 = new Date(contract.reportingStart);
+        let start = data.reportingStart;
+        let date1 = new Date(data.reportingStart);
         let date2 = new Date('2020-08-25');
 
         if (date1 < date2) {
-            console.log('save_renewable_energy_from_contracts reportingStart', contract.reportingStart);
             start = '2020-08-25';
         }
 
-        let query = await this.Query(`SELECT t.date::text FROM generate_series(timestamp '${contract.reportingStart}', timestamp '${contract.reportingEnd}', interval  '1 day') AS t(date);`);
+        let query = await this.Query(`SELECT t.date::text FROM generate_series(timestamp '${data.reportingStart}', timestamp '${data.reportingEnd}', interval  '1 day') AS t(date);`);
         let data_points = query?.rows;
-        let country = FormatNull(contract.countryRegionMap[0]?.country);
-
-        //INFO(`[SaveRenewableEnergyFromContracts] for ${miner} contract.id: ${id} , openVolume: ${totalEnergy}`);
+        let country = FormatNull(data.country);
 
         if (data_points && data_points?.length) {
             for (let i = 0; i < data_points.length; i++) {
                 let processed_date = data_points[i].date?.split(' ')[0];
                 let data = {
                     miner: miner,
-                    contract_id: id,
+                    allocation_cid: id,
                     energyWh: totalEnergy / data_points.length,
                     date: processed_date,
                     country: country,
@@ -76,97 +73,36 @@ export class DB {
 
                 try {
                     let values = `'${data.miner}', \
-                    '${data.contract_id}', \
+                    '${data.allocation_cid}', \
                     '${data.date}',\
                     '${data.energyWh}',\
                      ${data.country}`;
 
                     await this.Query(`
-            UPDATE fil_renewable_energy_from_contracts SET 
+            UPDATE fil_renewable_energy SET 
                             energyWh='${data.energyWh}'\
-                WHERE miner='${data.miner}' AND contract_id='${data.contract_id}' AND date='${data.date}' AND country=${data.country}; \
-            INSERT INTO fil_renewable_energy_from_contracts ( \
+                WHERE miner='${data.miner}' AND allocation_cid='${data.allocation_cid}' AND date='${data.date}' AND country=${data.country}; \
+            INSERT INTO fil_renewable_energy ( \
                 miner, \
-                contract_id, \
+                allocation_cid, \
                 date, \
                 energyWh, \
                 country \
                 ) \
-                SELECT ${values} WHERE NOT EXISTS (SELECT 1 FROM fil_renewable_energy_from_contracts WHERE miner='${data.miner}' AND contract_id='${data.contract_id}' AND date='${data.date}'  AND country=${data.country});`,
-                        'SaveRenewableEnergyFromContracts');
+                SELECT ${values} WHERE NOT EXISTS (SELECT 1 FROM fil_renewable_energy WHERE miner='${data.miner}' AND allocation_cid='${data.allocation_cid}' AND date='${data.date}'  AND country=${data.country});`,
+                        'SaveRenewableEnergy');
 
                 } catch (err) {
-                    WARNING(`[SaveRenewableEnergyFromContracts] -> ${err}`)
+                    WARNING(`[SaveRenewableEnergy] -> ${err}`)
                 }
 
             }
         }
     }
 
-    async save_renewable_energy_from_transactions(transaction) {
-        let id = transaction.id;
-        let miner = transaction.miner_id;
-        let totalEnergy = transaction.recsSoldWh;
-
-        let start = transaction.generation.generationStart;
-        let date1 = new Date(transaction.generation.generationStart);
-        let date2 = new Date('2020-08-25');
-
-        if (date1 < date2) {
-            console.log('save_renewable_energy_from_transactions generationStart', transaction.generation.generationStart);
-            start = '2020-08-25';
-        }
-
-        let query = await this.Query(`SELECT t.date::text FROM generate_series(timestamp '${start}', timestamp '${transaction.generation.generationEnd}', interval  '1 day') AS t(date);`);
-        let data_points = query?.rows;
-        let country = FormatNull(transaction.generation.country);
-
-        if (data_points && data_points?.length) {
-
-            for (let i = 0; i < data_points.length; i++) {
-                let processed_date = data_points[i].date?.split(' ')[0];
-                let data = {
-                    miner: miner,
-                    transaction_id: id,
-                    energyWh: totalEnergy / data_points.length,
-                    date: processed_date,
-                    country: country,
-                };
-
-                try {
-                    let values = `'${data.miner}', \
-                    '${data.transaction_id}', \
-                    '${data.date}',\
-                    '${data.energyWh}',\
-                     ${data.country}`;
-
-                    await this.Query(`
-            UPDATE fil_renewable_energy_from_transactions SET 
-                            energyWh='${data.energyWh}'\
-                WHERE miner='${data.miner}' AND transaction_id='${data.transaction_id}' AND date='${data.date}' AND country=${data.country}; \
-            INSERT INTO fil_renewable_energy_from_transactions ( \
-                miner, \
-                transaction_id, \
-                date, \
-                energyWh, \
-                country \
-                ) \
-                SELECT ${values} WHERE NOT EXISTS (SELECT 1 FROM fil_renewable_energy_from_transactions WHERE miner='${data.miner}' AND transaction_id='${data.transaction_id}' AND date='${data.date}' AND country=${data.country});`,
-                        'SaveRenewableEnergyFromTransactions');
-
-                } catch (err) {
-                    WARNING(`[SaveRenewableEnergyFromTransactions] -> ${err}`)
-                }
-
-            }
-        }
-    }
-
-    async refresh_renewable_energy_views() {
+    async refresh_renewable_energy_view() {
         try {
-            await this.Query("REFRESH MATERIALIZED VIEW CONCURRENTLY fil_renewable_energy_from_transactions_view_v4 WITH DATA;");
-            await this.Query("REFRESH MATERIALIZED VIEW CONCURRENTLY fil_renewable_energy_from_contracts_view_v4 WITH DATA;");
-            await this.Query("REFRESH MATERIALIZED VIEW CONCURRENTLY fil_renewable_energy_view_v4 WITH DATA;");
+            await this.Query("REFRESH MATERIALIZED VIEW CONCURRENTLY fil_renewable_energy_view_v5 WITH DATA;");
         } catch (err) {
             WARNING(`[RefreshRenewableEnergyMatViews] ${err}`)
         }
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import { RenewableEnergyPurchases } from '@filecoin-renewable-energy-purchases/j
 import { INFO, ERROR, WARNING } from './logs.js';
 import { DB } from './db.js';
 
+let db = new DB();
 let stop = false;
 
 async function run() {
@@ -10,7 +11,14 @@ async function run() {
     const allAllocations = await renewableEnergyPurchases.getAllAllocationsFromGithub();
     const allCertificateAllocation = await renewableEnergyPurchases.getAllCertificateAllocationsFromGithub();
 
-    console.log(allCertificateAllocation);
+    let count = 0;
+    for (const data of allAllocations) {
+        count++;
+        INFO(`Update RenewableEnergyPurchases [${count}/${allAllocations.length}]`);
+        await db.save_renewable_energy(data);
+    }
+
+    await db.refresh_renewable_energy_view();
 };
 
 const pause = (timeout) => new Promise(res => setTimeout(res, timeout * 1000));
@@ -20,8 +28,8 @@ const mainLoop = async _ => {
         while (!stop) {
             await run();
 
-            INFO(`Pause for 60 seconds`);
-            await pause(60);
+            INFO(`Pause for 24 hours`);
+            await pause(24 * 3600);
         }
 
     } catch (error) {
